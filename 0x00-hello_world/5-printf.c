a34a6d20736a9bc17da2281508af25140f7976db
KTTfx <nsromansarko@gmail.com>
2023-09-11 17:06:44 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program using printf to print a statement
+ * Return 0
+ */
+
+int main(void)
+(
+	printf("with proper grammar, but the outcome is a place of art, \n");
+	return(0);
