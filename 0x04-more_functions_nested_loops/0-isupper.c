4cc7ec6b1190144b970021bad5b6e290d9b8bc87
KTTfx <nsromansarko@gmail.com>
2023-09-14 15:12:11 -0700

    Upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for the uppercase character
+ * @c : parameter to be checked
+ * Return: 1 on success and 0 on failure
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= '2')
+		return (1);
+	else
+		return (0);
+}
