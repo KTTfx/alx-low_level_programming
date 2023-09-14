ba34779ffcd07fdf899ec837b41f70f623596e7f
KTTfx <nsromansarko@gmail.com>
2023-09-14 16:50:50 -0700

    6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints line on the terminal
+ * @n: number of time the character will be printed
+ * Return: void
+*/
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
