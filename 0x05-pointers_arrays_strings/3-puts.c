a8b18709f6630b2ae5a790c910ffa49ed0f8b08d
KTTfx <nsromansarko@gmail.com>
2023-09-21 14:27:44 -0700

    Print String

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints the string
+ * @str: Takes the value to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
+
